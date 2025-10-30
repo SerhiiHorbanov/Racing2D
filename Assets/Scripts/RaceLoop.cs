3ae5d95d7b6cd23ab97f2e5c4a0824b4c2b02e57
Serhiiko <serikgorbanev@gmail.com>
2025-10-30 18:08:08 +0100

    Added RaceLoop script

---
+++ Assets/Scripts/RaceLoop.cs
@@ -0,0 +1,70 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaceLoop : MonoBehaviour
+{
+	[SerializeField] private List<Checkpoint> _Checkpoints;
+
+	private Checkpoint _firstCheckpoint;
+	private Dictionary<RaceCar, int> _reachedCheckpointForCar = new();
+	
+	private void Awake()
+	{
+		if (_Checkpoints.Count == 0)
+		{
+			Debug.LogWarning("No checkpoints assigned");
+			return;
+		}
+		
+		_firstCheckpoint = _Checkpoints[0];
+
+		for (int i = 0; i < _Checkpoints.Count; i++)
+		{
+			Checkpoint checkpoint = _Checkpoints[i];
+			checkpoint.Index = i;
+			checkpoint.OnCarEntered += OnCarEnteredCheckpoint;
+		}
+		
+		
+	}
+
+	private void OnDestroy()
+	{
+		for (int i = 0; i < _Checkpoints.Count; i++)
+		{
+			Checkpoint checkpoint = _Checkpoints[i];
+			checkpoint.OnCarEntered -= OnCarEnteredCheckpoint;
+		}
+	}
+
+	private void OnCarEnteredCheckpoint(RaceCar car, int newCheckpoint)
+	{
+		if (!_reachedCheckpointForCar.TryGetValue(car, out int prevCheckpoint))
+			return;
+
+		bool shouldMoveToNextCheckpoint = prevCheckpoint == newCheckpoint - 1;
+		if (shouldMoveToNextCheckpoint)
+		{
+			_reachedCheckpointForCar[car] = newCheckpoint;
+			return;
+		}
+		
+		bool finishedALoop = prevCheckpoint == _Checkpoints.Count - 1 & newCheckpoint == 0;
+		if (finishedALoop)
+		{
+			_reachedCheckpointForCar[car] = 0;
+			CarFinishedALoop(car);
+		}
+	}
+
+	private void CarFinishedALoop(RaceCar car)
+	{
+		print($"car {car.gameObject.name} finished a loop");
+	}
+
+	public void AddCar(RaceCar car)
+	{
+		_reachedCheckpointForCar.Add(car, 0);
+	}
+}
